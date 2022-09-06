e0f5a3896830993860276558de353bae1322252c
Pooyan <thepooyan@gmail.com>
2022-09-06 11:25:44 +0430

    clicks added to pad

--- 0Export/app.js
+++ 0Export/app.js
@@ -30,6 +30,55 @@
   window.dc = dc;
 })()
 
+//* onclick system
+refreshOnClicks();
+function refreshOnClicks() {
+    let clicker = dc.queries("[data-onClick]");
+
+    clicker.forEach(item => {
+        if (item.getAttribute('data-group')) {
+
+            if (!item.clickEvent) {
+                item.clickEvent = true;
+                item.addEventListener("click", function () {
+                    dc.queries(`[data-group=${item.getAttribute('data-group')}`).forEach(item => {
+                        item.classList.remove(item.getAttribute("data-onClick"));
+                    })
+                    item.classList.toggle(item.getAttribute("data-onClick"));
+                });
+            }
+
+        } else {
+
+            if (!item.clickEvent) {
+                item.clickEvent = true;
+                item.addEventListener("click", function () {
+                    item.classList.toggle(item.getAttribute("data-onClick"));
+                });
+            }
+
+        }
+    })
+}
+
+//* Target system (grouped and single)
+let targeter = dc.queries('[data-target]');
+targeter.forEach(i => {
+    let target = dc.query(i.dataset.target);
+    if (target.dataset.group) {
+        i.addEventListener('click', function () {
+            dc.queries(`[data-group=${target.dataset.group}]`).forEach(item => {
+                item.classList.remove('active');
+            })
+            target.classList.add('active');
+        })
+    } else {
+        i.addEventListener('click', function () {
+            target.classList.toggle('active');
+        })
+    }
+})
+
 // Add smooth scrolling to all links
 $("a").on('click', function (event) {
   if (this.hash !== "") {
